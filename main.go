2a291d6474e032177402b511d3d84499a14bfb28
Larry Lu <pudding850806@gmail.com>
2018-10-08 21:01:00 +0800

    Finish basic proxy request to github

--- main.go
+++ main.go
@@ -1,11 +1,46 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 )
 
+func cloneRequest(r *http.Request) *http.Request {
+	method := r.Method
+	body := r.Body
+
+	path := r.URL.Path
+	rawQuery := r.URL.RawQuery
+	url := "https://github.com" + path + "?" + rawQuery
+
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		panic(err)
+	}
+	return req
+}
+
+func sendReqToUpstream(req *http.Request) []byte {
+	client := http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	resp.Body.Close()
+
+	return respBody
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
+	req := cloneRequest(r)
+	body := sendReqToUpstream(req)
+	w.Write(body)
 }
 
 func main() {
