ebb805e2ab500b77ad0b508b24573dc50f6fd294
Larry Lu <pudding850806@gmail.com>
2018-10-09 17:15:26 +0800

    Finish replaceURLInResp

--- main.go
+++ main.go
@@ -3,7 +3,31 @@ package main
 import (
 	"io/ioutil"
 	"net/http"
+	"regexp"
+	"strings"
 )
+
+func replaceURLInResp(body []byte, header http.Header) []byte {
+	// detect is html or not
+	contentType := header.Get("Content-Type")
+	isHTML := strings.Contains(contentType, "text/html")
+
+	// if NOT, return same body
+	if !isHTML {
+		return body
+	}
+
+	bodyStr := string(body)
+	bodyStr = strings.Replace(bodyStr, "https://github.com", "http://localhost:8080", -1)
+
+	re, err := regexp.Compile(`http://localhost:8080(.*)\.git`)
+	if err != nil {
+		panic(err)
+	}
+	bodyStr = re.ReplaceAllString(bodyStr, `https://github.com$1.git`)
+
+	return []byte(bodyStr)
+}
 
 func cloneRequest(r *http.Request) *http.Request {
 	method := r.Method
@@ -20,7 +44,7 @@ func cloneRequest(r *http.Request) *http.Request {
 	return req
 }
 
-func sendReqToUpstream(req *http.Request) []byte {
+func sendReqToUpstream(req *http.Request) ([]byte, http.Header) {
 	client := http.Client{}
 
 	resp, err := client.Do(req)
@@ -34,12 +58,15 @@ func sendReqToUpstream(req *http.Request) []byte {
 	}
 	resp.Body.Close()
 
-	return respBody
+	return respBody, resp.Header
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	req := cloneRequest(r)
-	body := sendReqToUpstream(req)
+
+	body, header := sendReqToUpstream(req)
+	body = replaceURLInResp(body, header)
+
 	w.Write(body)
 }
 
