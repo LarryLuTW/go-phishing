aeb1241468bbb4d463dcc18133602ce876ab0ec5
Larry Lu <pudding850806@gmail.com>
2018-10-31 10:58:24 +0800

    Finish implement insert and selectAll

--- db/db.go
+++ db/db.go
@@ -24,3 +24,23 @@ func Connect() {
 	db = _db
 	log.Println("Connect to db successfully")
 }
+
+// Insert save a string into db
+func Insert(s string) {
+	fishes := []byte("fishes")
+	db.RPush(fishes, []byte(s))
+}
+
+// SelectAll get all strings in db
+func SelectAll() []string {
+	fishes := []byte("fishes")
+	nFish, _ := db.LLen(fishes)
+	datas, _ := db.LRange(fishes, 0, int32(nFish))
+
+	strs := []string{}
+	for _, data := range datas {
+		strs = append(strs, string(data))
+	}
+
+	return strs
+}
